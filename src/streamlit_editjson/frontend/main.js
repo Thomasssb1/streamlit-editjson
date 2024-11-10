52515929e4ea24f63251378d87bac77100e1cb9e
Thomas Beer <thomas.beer04@outlook.com>
2024-11-10 04:53:06 +0000

    Detect values and create inputs which can be edited

--- src/streamlit_editjson/frontend/main.js
+++ src/streamlit_editjson/frontend/main.js
@@ -25,21 +25,68 @@ function onRender(event) {
     const jsonContainer = document.getElementById("json-container");
 
     let json = JSON.parse(json_str);
-    console.log(json);
-    console.log(json.split("\n"));
+    let labelledList = [];
+    let structures = ["{", "}", "[", "]", '"', ",", ":"];
 
-    console.log(`hi: ${json_str.split("\n").length}`);
-
-    for (let line of json_str.split("\n")) {
-      let lineDiv = document.createElement("pre");
-      lineDiv.innerText = line;
-      jsonContainer.appendChild(lineDiv);
-    }
+    labelledList = parse(json_str, structures, jsonContainer);
+    console.log(labelledList);
 
     Streamlit.setFrameHeight(json_str.split("\n").length * 30);
 
     window.rendered = true;
   }
+}
+
+function parse(data, structures, jsonContainer) {
+  function createInput(data) {
+    let lineDiv = document.createElement("input");
+    lineDiv.style.display = "inline-block";
+    lineDiv.innerText = data;
+    lineDiv.id = "input-" + data;
+    return lineDiv;
+  }
+
+  function createPre(data) {
+    let lineDiv = document.createElement("pre");
+    lineDiv.style.display = "inline-block";
+    lineDiv.innerText = data;
+    return lineDiv;
+  }
+
+  let labelledList = [];
+  let stringList = [];
+  let lastStructureIndex = -1;
+  for (let i = 0; i < data.length; i++) {
+    let current = data[i];
+    if (structures.includes(current)) {
+      if (current == '"') {
+        if (current == data[lastStructureIndex]) {
+          let stringData = stringList.join("");
+          labelledList.push(stringData);
+          jsonContainer.appendChild(createInput(stringData));
+        }
+        stringList = [];
+      } else if (current == ":") {
+        let stringData = labelledList[labelledList.length - 2];
+        let oldInput = document.getElementById("input-" + stringData);
+        jsonContainer.replaceChild(createPre(stringData), oldInput);
+      } else if (current == "," || current == "}") {
+        let stringData = stringList.join("").substring(1, stringList.length);
+        labelledList.push(stringData);
+        jsonContainer.appendChild(createInput(stringData));
+        stringList = [];
+      }
+      labelledList.push(current);
+      jsonContainer.appendChild(createPre(current));
+      lastStructureIndex = i;
+    } else if (
+      data[lastStructureIndex] == ":" ||
+      data[lastStructureIndex] == '"'
+    ) {
+      stringList.push(current);
+    }
+  }
+  return labelledList;
 }
 
 // Render the component whenever python send a "render event"
