601c7b665e9af1f113ff1f6ce61e271e66df7b0c
Thomasssb1 <thomas.beer04@outlook.com>
2026-03-23 21:32:22 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-editjson",
-    version="0.1.0",
+    version="1.0.0",
     author="Thomas Beer",
     author_email="thomas.beer04@outlook.com",
     description="edit and view json",
